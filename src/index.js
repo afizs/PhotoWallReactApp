8b5b3363218ffa39c43995b3cdf0a2a7b29b187d
Afiz S <itsafiz@gmail.com>
2018-12-03 17:35:38 +0530

    basic application

--- src/index.js
+++ src/index.js
@@ -1,44 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
-import "./styles.css";
-const items = [
-  "This is first item",
-  "Second Item",
-  "Third Item",
-  "Another item",
-  "Final Item"
-];
-
-class List extends Component {
-  render() {
-    return (
-      <ol>
-        {this.props.tasks.map((item, index) => (
-          <li key={index}>{item} </li>
-        ))}
-      </ol>
-    );
-  }
-}
-
-class Title extends Component {
-  render() {
-    return <h1>{this.props.title}</h1>;
-  }
-}
-
-class Main extends Component {
-  render() {
-    return (
-      <div>
-        <Title title="toDos: " />
-        <List tasks={items} />
-        <List tasks={items} />
-      </div>
-    );
-  }
-}
+import "./styles/styles.css";
+import Main from "./components/Main";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<Main />, rootElement);
