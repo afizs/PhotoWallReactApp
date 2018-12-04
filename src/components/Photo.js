552542ba71dc815c85825a28e13b7bfb5d0c3d83
Afiz S <itsafiz@gmail.com>
2018-12-04 15:45:53 +0530

    PropTypes added

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 function Photo(props) {
   const post = props.post;
@@ -22,5 +23,10 @@ function Photo(props) {
     </figure>
   );
 }
+
+Photo.propTypes = {
+  post: PropTypes.object.isRequired,
+  onRemovePhoto: PropTypes.func.isRequired
+};
 
 export default Photo;
--- src/components/PhotoWall.js
+++ src/components/PhotoWall.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import Photo from "./Photo";
 
 class PhotoWall extends Component {
@@ -16,4 +17,9 @@ class PhotoWall extends Component {
     );
   }
 }
+
+PhotoWall.propTypes = {
+  posts: PropTypes.array.isRequired,
+  onRemovePhoto: PropTypes.func.isRequired
+};
 export default PhotoWall;
